1e2b044bedb22d89da42b85e5f230bc084f581b8
Mac <mac@gind.local>
2026-09-26 02:34:28 +0700

    perf: run GA4 gtag in Partytown web worker to cut main-thread TBT

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -12,8 +12,56 @@ import SiteWrapperProvider from '@/container/SiteWrapperProvider'
 import { Toaster } from 'react-hot-toast'
 import NextNProgress from 'nextjs-progressbar'
 import themeJson from '@/../theme.json'
-import { GoogleAnalytics } from 'nextjs-google-analytics'
+import Script from 'next/script'
 import dynamic from 'next/dynamic'
+
+const GA_ID = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID
+
+// GA4 runs inside a Partytown web worker so gtag.js stays off the main thread (TBT)
+function Analytics() {
+	const router = useRouter()
+
+	React.useEffect(() => {
+		if (!GA_ID) return
+		const onRoute = () =>
+			(window as any).gtag?.('config', GA_ID, { page_path: router.asPath })
+		router.events.on('routeChangeComplete', onRoute)
+		return () => {
+			router.events.off('routeChangeComplete', onRoute)
+		}
+	}, [router])
+
+	if (!GA_ID) return null
+
+	return (
+		<>
+			<script
+				dangerouslySetInnerHTML={{
+					__html: `window.partytown={forward:['dataLayer.push','gtag']}`,
+				}}
+			/>
+			<Script
+				id="partytown"
+				src="/~partytown/partytown.js"
+				strategy="afterInteractive"
+			/>
+			<Script
+				id="ga4-src"
+				type="text/partytown"
+				strategy="afterInteractive"
+				src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+			/>
+			<Script
+				id="ga4-init"
+				type="text/partytown"
+				strategy="afterInteractive"
+				dangerouslySetInnerHTML={{
+					__html: `window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag('js',new Date());gtag('config','${GA_ID}');`,
+				}}
+			/>
+		</>
+	)
+}
 
 const DynamicSpeedInsights = dynamic(
 	() => import('@vercel/speed-insights/next').then(mod => ({ default: mod.SpeedInsights })),
@@ -62,7 +110,7 @@ export default function MyApp({ Component, pageProps }: AppProps) {
 
 	return (
 		<>
-			<GoogleAnalytics trackPageViews strategy="lazyOnload" />
+			<Analytics />
 
 			<DynamicSpeedInsights/>
 
